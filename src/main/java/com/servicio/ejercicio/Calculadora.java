2e3f6140b61fc69ff08694f6448a745690275570
magonzalezo <miguel.gonzalez.ordonez@gmail.com>
2020-01-24 01:47:14 -0500

    Se subre el proyecto base

---
+++ src/main/java/com/servicio/ejercicio/Calculadora.java
@@ -0,0 +1,21 @@
+package com.servicio.ejercicio;
+
+public class Calculadora {
+	
+	public int sumar (int operando_1, int operando_2) {
+		return operando_1 + operando_2;
+	}
+	
+	public int restar (int operando_1, int operando_2) {
+		return operando_1 - operando_2;
+	}
+	
+	public int multiplicar (int operando_1, int operando_2) {
+		return operando_1 * operando_2;
+	}
+	
+	public int dividir (int operando_1, int operando_2) {
+		return operando_1 / operando_2;
+	}
+	
+}
---
+++ src/main/java/com/servicio/ejercicio/Principal.java
@@ -0,0 +1,45 @@
+package com.servicio.ejercicio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Principal {
+	
+	@PostMapping("/suma")
+	public String sumar(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		Calculadora calculadora = new Calculadora();
+		return "Resultado de la suma: "+calculadora.sumar(num1, num2);
+	}
+	
+	@PostMapping("/resta")
+	public String restar(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		Calculadora calculadora = new Calculadora();
+		return "Resultado de la resta: "+calculadora.restar(num1, num2);
+	}
+	
+	@PostMapping("/multiplicacion")
+	public String multiplicar(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		Calculadora calculadora = new Calculadora();
+		return "Resultado de la multiplicacion: "+calculadora.multiplicar(num1, num2);
+	}
+	
+	@PostMapping("/division")
+	public String dividir(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		Calculadora calculadora = new Calculadora();
+		return "Resultado de la division: "+calculadora.dividir(num1, num2);
+	}
+	
+	@GetMapping("/prueba")
+	public String pruebaRetorno(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+		return "Usted envió: "+num1+" y "+num2;
+	}
+	
+	@GetMapping("/")
+	public String indice() {
+		return "Entro a la calculadora de su sufrimiento";
+	}
+	
+}
